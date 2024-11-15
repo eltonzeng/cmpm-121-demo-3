5e1956a06911d2858648a30cded24c91bb506381
Elton Zeng <ezeng2@ucsc.edu>
2024-11-15 14:01:41 -0800

    modified flyweight coin

--- src/flyweightCoin.ts
+++ src/flyweightCoin.ts
@@ -1,37 +1,36 @@
 export interface CoinData {
-    i: number;
-    j: number;
-    serial: number;
+  i: number;
+  j: number;
+  serial: number;
+}
+
+export class Coin implements CoinData {
+  i: number;
+  j: number;
+  serial: number;
+
+  constructor(i: number, j: number, serial: number) {
+    this.i = i;
+    this.j = j;
+    this.serial = serial;
   }
-  
-  export class Coin implements CoinData {
-    i: number;
-    j: number;
-    serial: number;
-  
-    constructor(i: number, j: number, serial: number) {
-      this.i = i;
-      this.j = j;
-      this.serial = serial;
-    }
-  
-    toString(): string {
-      return `${this.i}:${this.j}#${this.serial}`;
-    }
+
+  toString(): string {
+    return `${this.i}:${this.j}#${this.serial}`;
   }
-  
-  // Flyweight Factory for Coins
-  export class CoinFactory {
-    private coins: { [key: string]: Coin } = {};
-  
-    getCoin(i: number, j: number, serial: number): Coin {
-      const key = `${i}:${j}#${serial}`;
-      if (!this.coins[key]) {
-        this.coins[key] = new Coin(i, j, serial);
-      }
-      return this.coins[key];
+}
+
+// Flyweight Factory for Coins
+export class CoinFactory {
+  private coins: { [key: string]: Coin } = {};
+
+  getCoin(i: number, j: number, serial: number): Coin {
+    const key = `${i}:${j}#${serial}`;
+    if (!this.coins[key]) {
+      this.coins[key] = new Coin(i, j, serial);
     }
+    return this.coins[key];
   }
-  
-  export const coinFactory = new CoinFactory();
-  
+}
+
+export const coinFactory = new CoinFactory();
